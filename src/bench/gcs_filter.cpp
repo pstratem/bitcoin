bc2a50fb98f9d58f2929195af8c3d1c95f412e6e
Patrick Strateman <patrick.strateman@gmail.com>
2020-06-13 18:12:00 -0400

    Add DecodeGCSFilter benchmark

--- src/bench/gcs_filter.cpp
+++ src/bench/gcs_filter.cpp
@@ -39,5 +39,23 @@ static void MatchGCSFilter(benchmark::State& state)
     }
 }
 
+static void DecodeGCSFilter(benchmark::State& state)
+{
+    GCSFilter::ElementSet elements;
+    for (int i = 0; i < 10000; ++i) {
+        GCSFilter::Element element(32);
+        element[0] = static_cast<unsigned char>(i);
+        element[1] = static_cast<unsigned char>(i >> 8);
+        elements.insert(std::move(element));
+    }
+    GCSFilter filter({0, 0, 20, 1 << 20}, elements);
+    std::vector<unsigned char> encoded_filter = filter.GetEncoded();
+
+    while (state.KeepRunning()) {
+        GCSFilter filter({0, 0, 20, 1 << 20}, encoded_filter);
+    }
+}
+
 BENCHMARK(ConstructGCSFilter, 1000);
+BENCHMARK(DecodeGCSFilter, 25000);
 BENCHMARK(MatchGCSFilter, 50 * 1000);
